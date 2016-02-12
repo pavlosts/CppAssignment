2938da336539179e2ae094982008dbfba7843e77
MmarFr <MmarFr@users.noreply.github.com>
2016-02-12 14:40:32 +0200

    Updated to match the header file
    Updated .cpp to match the pointer array

--- toll.cpp
+++ toll.cpp
@@ -4,35 +4,39 @@
 
 using namespace std;
 
-Toll::Toll(int segID):
-curSeg(segID){
+Toll::Toll(int segID):curSeg(segID){
 	carsInQueue = rand() % 50;										//Number of new cars entering
 	for(int i = 0; i < carsInQueue; i++){
-		int destination = rand() % (NSegs - segID + 1) + segID		//Calculates a random number in current_segment < destination <= NSegs
-		carQueue[i] = {destination, -1, false};						//Adds a new car in queue
+		int dest = rand() % (NSegs - segID + 1) + segID;		//Calculates a random number in current_segment < destination <= NSegs
+		carQueuePtr[i] = new Car(dest);						//Adds a new car in queue
 	}
 	for(int i = carsInQueue; i < 50; i++){
-		carQueue[i] = NULL;											//Initilizes the rest of the queue
+		carQueuePtr[i] = NULL;											//Initilizes the rest of the queue
 	}
 }
 
 Toll::~Toll(){
+	for(int i = 0; i < carsInQueue; i++){
+       		delete carQueuePtr[i];		//Deletes cars in queue
+	}
 	cout << "Toll destructed." << endl;
 }
 
-Toll::addCars( void ){
+void Toll::addCars( void ){
+	cout<< "This toll queue had " << carsInQueue << " cars." << endl;
+	//!!!!!!!!! this rand might be a problem, please revise
 	int newCars = rand() % (50 - carsInQueue);						//Number of new cars entering in carsInQueue < newCars < 50
 	for(int i = carsInQueue; i < carsInQueue + newCars; i++){
-		int destination = rand() % (NSegs - segID + 1) + segID		//Calculates a random number in current_segment < destination <= NSegs
-		carQueue[i] = {destination, -1, false}; 					//Adds a new car in queue
+		int destination = rand() % (NSegs - segID + 1) + NSegs		//Calculates a random number in current_segment < destination <= NSegs
+		carQueuePtr[i] = new Car(destination);						//Adds a new car in queue
 	}
+	cout<< "It now has " << carsInQueue << " cars." <<endl;
 }
 
-/*Δεν είμαι σίγουρος αν είναι σωστή.
-	1) Δεν ξέρω αν ο τρόπος με το NULL είναι σωστός.
-	2) Αφαιρεί το τελευταίο αυτοκίνητο από την ουρα. Θεωρητικά
-		δεν είναι σωστό αλλά δεν νομίζω οτι μας ενοχλεί σε κάτι*/
-Toll::removeCar( void ){
-	carQueue[carsInQueue] = NULL;									//Removes the last car in queue
+
+Car* Toll::removeCar( void ){
+	Car* removed_car = carQueuePtr[carsInQueue]; //points to removed car
+	carQueuePtr[carsInQueue] = NULL;			//pointer in carqueue set to null
 	carsInQueue--;
+	return removed_car;
 }
