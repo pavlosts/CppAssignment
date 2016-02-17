a7f812b43ec28dd063be25098736ea6fc9b27d42
MmarFr <MmarFr@users.noreply.github.com>
2016-02-17 15:10:11 +0200

    Added int carsPassed+ functions
    Added a new field which counts the number of cars that passed through the toll in one round of simulation,
    added function set_carsPassed, which sets the number of cars that passed to zero.
    added function get_carsPassed which returns the value of carsPassed for the toll.

--- toll.h
+++ toll.h
@@ -9,11 +9,14 @@ private:
 	int carsInQueue;			//Number of cars waiting
 	Car* carQueuePtr[100];			//Array of pointers to cars
 	int curSeg;
+	int carsPassed;             //Counts the number of cars passed from toll
 public:
 	Toll( int segID );			//segNum = current segment's ID
 	~Toll();
 	void addCars( void );		//Adds random number of cars in queue
 	Car* removeCar( void );		//Removes a car from queue
+	void set_carsPassed(int cars =0){ carsPassed = cars;};
+	int get_carsPassed(){ return carsPassed;};
 }
 
 #endif
