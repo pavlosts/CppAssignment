2af1a898914a056e8e69bb22a31f1e12b048aa30
pavlosts <pavlos_tsaou@hotmail.gr>
2016-02-19 01:06:24 +0200

    Added files via upload

---
+++ highway.h
@@ -0,0 +1,19 @@
+#ifndef HIGHWAY_H
+#define HIGHWAY_H
+
+#include <iostream>
+#include <string>
+
+class Highway{
+private:
+	int percent;
+	int numberOfSegs;
+	Segment *segments;
+	int cars;
+public:
+	Highway(int NSegs, int K, int P);
+	~Highway();
+	void operate();	
+};
+
+#endif
