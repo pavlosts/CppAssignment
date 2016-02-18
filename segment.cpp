0b8976a9ce0874144a632361782f121f6a11d2bc
MmarFr <MmarFr@users.noreply.github.com>
2016-02-18 13:06:33 +0200

    Update segment.cpp
    Στον constructor
    9  -- Πρεπει το prev και το next να αρχικοποιουνται ως NULL για σωστη μοντελοποιηση
    15-- Πρεπει ο constructor να παρει τη θεση του αυτοκινητου, αλλιως το αρχικοποιει εκτως αυτοκινητοδρομου (λαθος)
    17-- Πρεπει να κανουμε τις υπολοιπες θεσεις NULL
    23-- Η καινουρια συναρτηση
    58--Δεν μπορεις να εχεις access στο private του car!

--- segment.cpp
+++ segment.cpp
@@ -4,61 +4,76 @@
 
 using namespace std;
 
-Segment::Segment(int k, int id, Segment &s):segID(id){
+Segment::Segment(int k, int id):segID(id){
 	gate = new Entrance(k, id);
-	prevSeg = s;
+	prevSegPtr = NULL;
+	nextSegPtr = NULL;
 	capacity = (rand() % 200) + 300;			// 0 + 300 <= capacity <= 200 + 300
 	numberOfCars = rand() % capacity;
 	for(int i = 0; i < numberOfCars; i++){
 		int dest = rand() % (NSegs - segID + 1) + segID;
-		carPtr[i] = new Car(dest);
+		cars_in_seg[i] = new Car(dest, id);  //the current position of the car is the id of this segment
 	}
+	for(int i=numberOfCars; i<capacity; i++){ //the rest of the spots are empty
+        cars_in_seg[i] = NULL;
+	}
+}
+
+
+void Segment::Insert_seg_behind(Segment* s){
+    prevSegPtr = s;
+    s->nextSegPtr = this;
 }
 
 Segment::~Segment(){
 	for(int i = 0; i < numberOfCars; i++){
-		delete carPtr[i];
+		delete cars_in_seg[i];
 	}
+	delete gate;
+	cout>> "Segment deleted!">>endl;
 }
 
-void Segment::enter(Car &enteringCar){
-	int numberOfNewCars;
-	Car *newCars[500];				//An array for new cars
-	gate.operate(newCars, capacity - numberOfCars);		//Gets the cars to enter
-	for(int i = 0; i < capacity - numberOfCars; i++){	//Counts the new cars
+void Segment::enter(int empty_spots){
+	int numberOfNewCars=0;
+	Car* newCars[empty_spots];				//An array for new cars as big as the empty spots we have
+	gate.operate(empty_spots, newCars[]);		//Gets the cars from tolls
+	for(int i = (empty_spots-1); i >= 0; i--){	//Counts the new cars from BACK to front of array
 		if(newCars != NULL){
 			numberOfNewCars++;
+			/*η επαναληψη σου δεν δουλευε, οποτε το εβαλα εδω*/
+			cars_in_seg[numberOfCars] = newCars[i];  //add the new car in the segment
+			numberOfCars++;         //number of cars is now greater
 		}else{
 			break;
 		}
 	}
-	for(int i = numberOfCars; i < numberOfCars + numberOfNewCars; i++){	//Adds the new cars
-		carPtr[i] = newCars[i - numberOfNewCars];
-	}
+	/*ΠΡΟΣΟΧΗ! Αν το κανουμε αυτο, σβηνουμε τα αυτοκινητα που χρειαζομαστε!!
 	for(int i = 0; i < numberOfNewCars; i++){					//Deletes the temp array
 		delete newCars[i];
-	}
+	}*/
 }
 
 void Segment::exit(){
-	for(int i = numberOfCars - 1; i >= 0; i++ ){
-		if(carPtr[i].ready == true && carPtr[i].destination == segID){
-			delete carPtr[i];
+	for(int i = numberOfCars - 1; i >= 0; i-- ){
+		if((cars_in_seg[i]->is_ready()) && (cars_in_seg[i]->destination_reached())){
+			delete cars_in_seg[i];
+			cout<< "A car has exited the highway" << endl;
+			cars_in_seg[i] = NULL;
+			numberOfCars--;
 		}
 	}
 }
 
 void Segment::pass(){
 	for(int i = numberOfCars - 1; i >= 0; i++){
-		if(carPtr[i].ready == true && carPtr[i].destination > segID){
-			nextSeg.enter(carPtr[i]);
+		if((cars_in_seg[i]->is_ready()) && !(cars_in_seg[i]->destination_reached())){
+			 //car enters next segment
+			/*nextSeg.enter(carPtr[i]); εκτος και αν κανεις override τη συναρτηση, η εντολη αυτη οδηγει στην καταστροφη :Ρ*/
 		}
 	}
 }
 
-int Segment::getNoOfVehicles(){
-	return numberOfCars;
-}
+
 
 void Segment::operate(){
 
