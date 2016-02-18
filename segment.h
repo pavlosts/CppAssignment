6aecc8f11979d0508d1e60f659790e0e386e9505
MmarFr <MmarFr@users.noreply.github.com>
2016-02-18 12:51:44 +0200

    Update segment.h
    -> Αλλαξα το ονομα του πινακα σε cars_in_seg (πιο χαρακτηριστικο)
    
    ->Τα nextSeg, prevSeg ειναι δεικτες, για να υλοποιειται η διπλα συνδεδεμενη λιστα που λεγαμε
    
    ->Εγινε ενα μπλεξιμο μαλλον εκει... Η enter φερνει τα αυτοκινητα ΑΠΟ ΤΑ ΔΙΟΔΙΑ
    
    ->H getNoOfVehicles() πρεπει να ειναι inline
    
    ->Nεα συναρτηση, η Insert_seg_behind() βοηθαει στην υλοποιηση της συνδεδεμενης λιστας. Την καλουμε με ενα seg2ptr και βαζουμε ορισμα ενα seg1ptr..... [δηλ. 'seg2ptr->Insert_seg_behind(seg1ptr)' ] 
    Αποτελεσμα: To 2 μπαινει πισω απο το 1.

--- segment.h
+++ segment.h
@@ -8,19 +8,20 @@ class Segment{
 private:
 	Entrance gate;
 	int numberOfCars;
-	Car* carPtr[500];
+	Car* cars_in_seg[500];
 	int capacity;
-	Segment nextSeg;
-	Segment PrevSeg;
+	Segment* nextSegPtr;
+	Segment* PrevSegPtr;
 	int segId;
 public:
 	Segment(int k, int id);
 	~Segment();
-	void enter(Car &enteringCar);
+	void enter(int); //enters the cars FROM OUTSIDE
 	void exit();
 	void pass();
-	int getNoOfVehicles();
+	int getNoOfVehicles(){return numberOfCars;};
 	void operate();
+	void Insert_seg_behind(Segment*); //inserts a segment behind the seg its called with
 }
 
 #endif
